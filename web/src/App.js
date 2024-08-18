c770212090f1936d81c449b0b181c6629df3fe28
Omar Hammad <hammado92@gmail.com>
2024-08-18 22:27:42 +0300

    fix data path

--- web/src/App.js
+++ web/src/App.js
@@ -5,14 +5,26 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/chapters.json')
-      .then(response => response.json())
-      .then(data => setData(data));
+    fetch('/hci_course/chapters.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.indexOf("application/json") !== -1) {
+          return response.json();
+        } else {
+          throw new Error("Received non-JSON response");
+        }
+      })
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
+  
 
   return (
     <div className="App">
-      <h1>SWE 503: HCI Course Plan</h1>
+      <h1>SWE 595: Special Topics in Software Engineering - Human-Computer Interaction (HCI)</h1>
       <table>
         <thead>
           <tr>
@@ -24,24 +36,38 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{item.topic}</td>
-              <td>
-                <ul>
-                  {item.chapters.map((chapter, idx) => (
-                    <li key={idx}>{chapter.title} <a href={chapter.slides} target="_blank" rel="noopener noreferrer">
-                      (ppt)
-                    </a>
-                    </li>
-                  ))}
-                </ul>
-              </td>
-              <td></td>
-              <td></td>
+          {data.length > 0 ? (
+            data.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.topic}</td>
+                <td>
+                  <ul>
+                    {item.chapters && item.chapters.length > 0 ? (
+                      item.chapters.map((chapter, idx) => (
+                        <li key={idx}>
+                          {chapter.title}
+                          {chapter.slides && (
+                            <a href={chapter.slides} target="_blank" rel="noopener noreferrer">
+                              (ppt)
+                            </a>
+                          )}
+                        </li>
+                      ))
+                    ) : (
+                      <li>No chapters available</li>
+                    )}
+                  </ul>
+                </td>
+                <td></td>
+                <td></td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">No data available</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
